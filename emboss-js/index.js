b22dcd09f51ff1688651c032155cdf1a50f5ce89
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2017-03-07 18:09:07 +0000

    remove duplicate dir

--- emboss-js/index.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-
-    getProteinStats: require('./lib/pepstats'),
-    translateSequence: require('./lib/transeq'),
-    alignSequencePair: require('./lib/water')
-
-}
-
--- emboss-js/lib/eof.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = new Buffer([ 0x0A, 0x04 ])
-
-
